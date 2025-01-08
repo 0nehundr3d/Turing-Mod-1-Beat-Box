b524dd339c0222e2d034a7a4c881ed02229ded6e
ontruster74 <ontruster74@gmail.com>
2025-01-08 15:14:58 -0700

    complete Iteration 1

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,15 +1,44 @@
 class LinkedList
   attr_reader :head
 
-  def append
-    
+  def append(data)
+    if !@head
+      @head = Node.new(data)
+    else
+      current_node = @head
+      while true do
+        if current_node.next_node == nil
+          current_node.next_node = Node.new(data)
+          break
+        end
+        current_node = current_node.next_node
+      end
+    end
   end
 
   def count
-    
+    count = 0
+    current_node = @head
+
+    while true do
+      count += 1
+      break if current_node.next_node == nil 
+      current_node = current_node.next_node
+    end
+
+    return count
   end
 
   def to_string
-    
+    string = ""
+    current_node = @head
+
+    while true do
+      string << current_node.data << " "
+      break if current_node.next_node == nil
+      current_node = current_node.next_node
+    end
+
+    return string.chop
   end
 end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -18,19 +18,27 @@ describe LinkedList do
   end
 
   describe '#append' do
-    @list.append("doop")
-    expect(@list.head).to be_a(Node)
-    expect(@list.head.data).to eq("doop")
+    it 'can append nodes to the list' do
+      @list.append("doop")
+      expect(@list.head).to be_a(Node)
+      expect(@list.head.data).to eq("doop")
+    end
   end
 
   describe '#count' do
-    @list.append("doop")
-    expect(@list.count).to eq(1)
+    it 'can give a count of elements in the list' do
+      @list.append("doop")
+      @list.append("deep")
+      expect(@list.count).to eq(2)
+    end
   end
 
   describe '#to_string' do
-    @list.append("doop")
-    expect(@list.to_string).to eq("doop")
+    it 'can render the list data as a string' do
+      @list.append("doop")
+      @list.append("deep")
+      expect(@list.to_string).to eq("doop deep")
+    end
   end
-  
+
 end
